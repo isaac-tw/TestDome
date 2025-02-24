922e6e928d1c99d6e7fa9fa40fcccce3ee245778
Isaac Huang <isaac.huang.tw@gmail.com>
2025-02-24 21:48:35 +0800

    Add JS - 'Song'

---
+++ JavaScript/Song/main.js
@@ -0,0 +1,34 @@
+// https://www.testdome.com/questions/javascript/song/110584
+
+class Song {
+  name;
+  nextSong;
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  /**
+   * @return {boolean} true if the playlist is repeating, false if not.
+   */
+  isInRepeatingPlaylist() {
+    let slow = this;
+    let fast = this;
+
+    while (fast && fast.nextSong) {
+      slow = slow.nextSong;
+      fast = fast.nextSong.nextSong;
+
+      if (slow === fast) return true;
+    }
+    return false;
+  }
+}
+
+let first = new Song("Hello");
+let second = new Song("Eye of the tiger");
+
+first.nextSong = second;
+second.nextSong = first;
+
+console.log(first.isInRepeatingPlaylist());
